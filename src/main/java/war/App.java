46d81d80de758f587443cd41ce95b8b6103d2620
zaheer-1988 <51013358+zaheer-1988@users.noreply.github.com>
2019-06-29 13:49:59 +0530

    Update App.java

--- src/main/java/war/App.java
+++ src/main/java/war/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "My name is Zaheer Syed. This is my first deploy in single step" );
     }
 }
